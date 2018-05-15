3aaaa341824f145164222a0bf1348b5b3f5745dd
Alexander Popov <aleksander.popov.93@gmail.com>
2018-05-15 12:29:18 +0300

    small fixes and add gitignore

--- storage/index.ts
+++ storage/index.ts
@@ -24,6 +24,7 @@ export namespace Storage {
     }
 
     export function popData(uuid: string): string {
+        throwIfNotTruthy('Uuid', uuid);
         const rawSessionData = sessionsMap[uuid];
         if (!rawSessionData) throw new Error(`No data present with id ${uuid}`);
 
@@ -35,20 +36,31 @@ export namespace Storage {
     }
 
     export function startSuite(uuid: string, suiteName: string, timestamp: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Timestamp', timestamp);
+        throwIfNotTruthy('Suite name', suiteName);
         ensurePresent(uuid);
         getSession(uuid).startSuite(suiteName, parseInt(timestamp));
     }
 
     export function endSuite(uuid: string, timestamp: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Timestamp', timestamp);
         getSession(uuid).endSuite(parseInt(timestamp));
     }
 
     export function startCase(uuid: string, testName: string, timestamp: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Test name', testName);
+        throwIfNotTruthy('Timestamp', timestamp);
         getSession(uuid).startCase(testName, parseInt(timestamp));
     }
 
     export function endCase(uuid: string, rawStatus: string, rawError: string, timestamp: string) {
-        const error = rawError ? JSON.parse(rawError) : null;
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Status', rawStatus);
+        throwIfNotTruthy('Error', rawError);
+        throwIfNotTruthy('Timestamp', timestamp);
         const testStatus = rawStatus === 'broken'
             ? TEST_STATUS.BROKEN
             : rawStatus === 'failed'
@@ -57,19 +69,29 @@ export namespace Storage {
                     ? TEST_STATUS.PASSED
                     : rawStatus === 'pending'
                         ? TEST_STATUS.PENDING : TEST_STATUS.SKIPPED;
+        const error = rawError ? JSON.parse(rawError) : null;
         getSession(uuid).endCase(testStatus, error, parseInt(timestamp));
     }
 
     export function startStep(uuid: string, stepName: string, timestamp: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Step name', stepName);
+        throwIfNotTruthy('Timestamp', timestamp);
         getSession(uuid).startStep(stepName, parseInt(timestamp));
     }
 
     export function endStep(uuid: string, rawStatus: string, timestamp: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Status', rawStatus);
+        throwIfNotTruthy('Timestamp', timestamp);
         const stepStatus = rawStatus === 'failed' ? STEP_STATUS.FAILED : STEP_STATUS.PASSED;
         getSession(uuid).endStep(stepStatus, parseInt(timestamp));
     }
 
     export function setDescription(uuid: string, content: string, type: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Content', content);
+        throwIfNotTruthy('Type', type);
         const descriptionType = type === 'html'
             ? DESCRIPTION_TYPE.HTML
             : type === 'text'
@@ -78,16 +100,33 @@ export namespace Storage {
         getSession(uuid).setDescription(content, descriptionType);
     }
 
-    export function addAttachment(uuid: string, attachmentName: string, stringBuffer: string, type: string) {
-        getSession(uuid).addAttachment(attachmentName, stringBuffer, type);
+    export function addAttachment(uuid: string, name: string, stringBuffer: string, type: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Name', name);
+        throwIfNotTruthy('Content', stringBuffer);
+        throwIfNotTruthy('Type', type);
+        getSession(uuid).addAttachment(name, stringBuffer, type);
     }
 
     export function addLabel(uuid: string, name: string, value: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Name', name);
+        throwIfNotTruthy('Value', value);
         getSession(uuid).addLabel(name, value);
     }
 
     export function addParameter(uuid: string, kind: string, name: string, value: string) {
+        throwIfNotTruthy('Uuid', uuid);
+        throwIfNotTruthy('Kind', kind);
+        throwIfNotTruthy('Name', name);
+        throwIfNotTruthy('Value', value);
         getSession(uuid).addParameter(kind, name, value);
+    }
+
+    function throwIfNotTruthy(name: string, value: any) {
+        if (!value) {
+            throw new Error(`${name} should be truthy, but was '${value}'`);
+        }
     }
 
     function transformCachedData(cachedSession: CachedSession): MemorySession {
